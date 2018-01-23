97ecd61453fa37815bf5a1f8086b5a4f9fae49f4
Stanley Zheng <dev.stanzheng@gmail.com>
2018-01-23 17:40:37 -0500

    adding dynamo python helper

---
+++ helpers.py
@@ -0,0 +1,32 @@
+from decimal import Decimal
+import json
+
+# https://github.com/boto/boto3/issues/665#issuecomment-286130739
+def decode_object_hook(dct):
+    for key, val in dct.items():
+        if isinstance(val, float):
+            dct[key] = Decimal(str(val))
+        if isinstance(val, list):
+            val = [Decimal(str(i)) for i in val]
+            dct[key] = [Decimal(str(i)) for i in val]
+        try:
+            dct[key] = TypeSerializer().serialize(val)
+        except:
+            dct[key] = val
+    return dct
+
+
+def json_serial(val):
+    if isinstance(val, datetime):
+        serial = val.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        return serial
+    elif isinstance(val, set):
+        serial = list(val)
+        return serial
+    elif isinstance(val, uuid.UUID):
+        serial = str(val.hex)
+        return serial
+
+def dumps(dct, *args, **kwargs):
+    kwargs['object_hook'] = decode_object_hook
+    return json.loads(json.dumps(dct, default=json_serial), *args, **kwargs)
